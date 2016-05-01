6caba2b5ab8efc238e54d676967d7a3cc4e54a8d
Fred Jiang <554129592@qq.com>
2016-05-01 21:16:25 +0800

    add source codes

---
+++ Assets/Data.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace dpp
+{
+    public class Data : MonoBehaviour
+    {
+        public int no { get; set; }
+        public double cj { get; set; }
+        public double cw { get; set; }
+        public double deg { get; set; }
+
+        public Data(int n, double j, double w, double d)
+        {
+            no = n;
+            cj = j;
+            cw = w;
+            deg = d;
+        }
+    }
+}
